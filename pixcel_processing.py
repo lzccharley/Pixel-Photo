69db4eff143b3e76b09bfc88ee26c1dc765fbe3e
lzccharley <56666321+lzccharley@users.noreply.github.com>
2022-09-12 22:23:18 +0800

    update

--- pixcel_processing.py
+++ pixcel_processing.py
@@ -24,6 +24,11 @@ PIXEL_BASE = 64
 
 class PixelProcessing:
     def __init__(self, img_root=IMG_ROOT, pixel_root=PIXEL_ROOT, is_clear=True):
+        """
+        :param img_root: 拼图素材图像目录
+        :param pixel_root: 像素图像保存目录
+        :param is_clear: 是否清空像素素材目录
+        """
         self.IMG_ROOT = img_root
         self.Pixel_ROOT = pixel_root
         if not os.path.exists(self.Pixel_ROOT):  # 不存在像素目录
@@ -34,6 +39,11 @@ class PixelProcessing:
         self.imgs_name = os.listdir(self.IMG_ROOT)
 
     def processing1(self, size=(PIXEL_BASE, PIXEL_BASE)):
+        """
+        处理像素图像
+        :param size: 像素素材的裁剪大小
+        :return:
+        """
         for img_name in self.imgs_name:
             img = cv2.imread(os.path.join(self.IMG_ROOT, img_name))
             img = cv2.resize(img, size)
--- pixel_fusion.py
+++ pixel_fusion.py
@@ -26,6 +26,11 @@ TARGET_REDUCTION = 16
 
 class PixelFusion:
     def __init__(self, pixel_root=PIXEL_ROOT, target_root=TARGET_ROOT, result_root=RESULT_ROOT):
+        """
+        :param pixel_root: 拼图素材目录
+        :param target_root: 目标图像目录
+        :param result_root: 结果保存目录
+        """
         self.pixel_root = pixel_root
         self.target_root = target_root
         self.result_root = result_root
@@ -41,6 +46,11 @@ class PixelFusion:
         # print(self.pixels_tree.query([22249, 23341, 24678]))
 
     def rgb_fusion(self, reduction=TARGET_REDUCTION):
+        """
+        使用rgb最相近的图片来拼出目标图片
+        :param reduction: 目标图像的缩放比例
+        :return:
+        """
         for target_name in tqdm(self.targets_name):
             target = cv2.imread(os.path.join(self.target_root, target_name))
             height = target.shape[0] // reduction
